434af0653239bea4866a42b384ef1ee2366dd121
Arnav Gupta <arnav@codingblocks.com>
2018-01-08 18:16:28 +0530

    node8
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ tests/nodejs8/script.js
@@ -0,0 +1,11 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function (line) {
+  console.log("Hello " + line);
+});
