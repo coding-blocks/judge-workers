2b1ea05f57a7a480c24e0fa6fd7810fdd4f16070
Arnav Gupta <arnav@codingblocks.com>
2018-07-06 17:50:31 +0530

    escape ++
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- build_workers.sh
+++ build_workers.sh
@@ -5,7 +5,7 @@ DIR=$(cd -)
 for i in $(ls "$DIR/containers")
 do
     cd $DIR/containers/$i
-    #docker image rm codingblocks/judge-worker-$i
+    docker image rm codingblocks/judge-worker-$i
     docker build -t codingblocks/judge-worker-$i .
     cd $DIR
 done
