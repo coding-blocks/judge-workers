8bcdc7dd66c1bd6b9837f90b00c97e201fc2f96d
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2018-07-11 07:14:38 +0530

    Golang Support

---
+++ tests/golang/test.go.py
@@ -0,0 +1,14 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "os"
+        )
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    text, _ := reader.ReadString('\n')
+    fmt.Println("Hello"+text)
+
+}
