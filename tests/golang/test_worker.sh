fa6914b24c72cb6759248242461eaf3f6fe31dcd
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-07-11 15:34:24 +0530

    golang support

---
+++ tests/golang/test_worker.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+pushd $(dirname "$0")
+DIR=$(pwd)
+RUNBOX="${DIR}/runbox"
+
+echo $RUNBOX
+# Create runbox
+mkdir -p $RUNBOX
+
+# Copy source to runbox
+cp $DIR/main.go $RUNBOX/main.go
+cp $DIR/run.stdin $RUNBOX/run.stdin
+
+# Test Compile
+docker run \
+    --cpus="1" \
+    --memory="100m" \
+    --ulimit nofile=64:64 \
+    --rm \
+    --read-only \
+    -v "$RUNBOX":/usr/src/runbox \
+    -v "$RUNBOX":/tmp \
+    -w /usr/src/runbox codingblocks/judge-worker-golang \
+    bash -c "/bin/compile.sh && /bin/run.sh"
+
+ls -lh ${RUNBOX}
+
+expected="Hello World"
+actual="$(cat ${RUNBOX}/run.stdout)"
+if [ "$expected" == "$actual" ] ;then
+    :
+else
+    echo "MISMATCH: Expected = $expected; Actual = $actual"
+    exit 1
+fi
+
+# Delete runbox
+rm -rf $RUNBOX
