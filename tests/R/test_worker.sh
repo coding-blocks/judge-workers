3ae79296eea77827022b42bfacf628f2b8731857
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-07-29 14:54:14 +0530

    fix

--- tests/R/test_worker.sh
+++ tests/R/test_worker.sh
@@ -11,7 +11,7 @@ rm -rf $RUNBOX
 mkdir -p $RUNBOX
 
 # Copy source to runbox
-cp -fv $DIR/script.r $RUNBOX/script.r
+cp -fv $DIR/script.R $RUNBOX/script.R
 cp -fv $DIR/run.stdin $RUNBOX/run.stdin
 
 # Test Compile
@@ -23,7 +23,7 @@ docker run \
     --read-only \
     -v "$RUNBOX":/usr/src/runbox \
     -v "$RUNBOX":/tmp \
-    -w /usr/src/runbox codingblocks/judge-worker-R \
+    -w /usr/src/runbox codingblocks/judge-worker-r \
     bash -c "/bin/compile.sh && /bin/run.sh"
 
 ls -lh ${RUNBOX}
