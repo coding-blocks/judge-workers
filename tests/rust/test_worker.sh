64c6016a1626b2d142fb7f5b62d8db8d37cbea1f
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2018-08-01 19:14:25 +0530

    added test

--- tests/rust/test_worker.sh
+++ tests/rust/test_worker.sh
@@ -23,7 +23,7 @@ docker run \
     --read-only \
     -v "$RUNBOX":/usr/src/runbox \
     -v "$RUNBOX":/tmp \
-    -w /usr/src/runbox rust \
+    -w /usr/src/runbox codingblocks/judge-worker-rust \
     bash -c "/bin/compile.sh && /bin/run.sh"
 
 ls -lh ${RUNBOX}
